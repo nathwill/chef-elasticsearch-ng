e5d4daef8247bfdeba288d1338ad4d6f53f3cd66
Nathan Williams <nathan@teamtreehouse.com>
2015-05-08 10:30:16 -0700

    bump ver

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+
+# 0.2.0 / 2015-05-08
+
+* add cluster recipe
+* fill out the README
+* add integration tests
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'nath.e.wilL@gmail.com'
 license          'apache2'
 description      'Installs/Configures elasticsearch-ng'
 long_description 'Installs/Configures elasticsearch-ng'
-version          '0.1.1'
+version          '0.2.0'
 
 depends 'apt'
 depends 'yum'
