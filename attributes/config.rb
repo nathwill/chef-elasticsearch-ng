51a9366733cae0fe4898a0ce21f365f8d4517e25
Nathan Williams <nathan@teamtreehouse.com>
2015-05-08 10:19:43 -0700

    a bit more opinionated default configuration

--- attributes/config.rb
+++ attributes/config.rb
@@ -1,4 +1,14 @@
 default['elasticsearch']['config'].tap do |c|
   c['cluster.name'] = node.chef_environment
   c['node.name'] = node.name
+
+  c['http.port'] = 9_200
+  c['transport.tcp.port'] = 9_300
+  c['discovery.zen.ping.multicast.port'] = 54_328
+
+  c['node.max_local_storage_nodes'] = 1
+  c['bootstrap.mlockall'] = true
+  c['action.auto_create_index'] = true
+  c['action.disable_delete_all_indices'] = true
+  c['script.disable_dynamic'] = true
 end
