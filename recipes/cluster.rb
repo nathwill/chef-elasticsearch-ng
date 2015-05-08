54f54c92d179fcc36179b63f0b11b306de38514d
Nathan Williams <nathan@teamtreehouse.com>
2015-05-08 10:23:03 -0700

    fix cops

--- recipes/cluster.rb
+++ recipes/cluster.rb
@@ -9,9 +9,9 @@ end
 
 node.default['elasticsearch']['config'].tap do |c|
   c['gateway.expected_nodes'] = es_nodes.length
-  c['discovery.zen.minimum_master_nodes'] = (es_nodes.length / 2 ).floor + 1
+  c['discovery.zen.minimum_master_nodes'] = (es_nodes.length / 2).floor + 1
   c['discovery.zen.ping.unicast.hosts'] = es_nodes.inspect
-  c["discovery.zen.ping.multicast.enabled"] = false
+  c['discovery.zen.ping.multicast.enabled'] = false
 end
 
 include_recipe cookbook_name
