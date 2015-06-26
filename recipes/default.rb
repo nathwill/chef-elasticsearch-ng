7fc8a0c556e03b3379db0826094ddcf954f46432
ynuyasha <ynuyasha@users.noreply.github.com>
2015-06-26 11:28:10 +0200

    fix syntax error

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,7 @@
 # Recipe:: default
 #
 
-Chef::Recipe, Chef::Resource].each { |l| l.send :include, ::Extensions }
+[Chef::Recipe, Chef::Resource].each { |l| l.send :include, ::Extensions }
 
 %w( repo install configure service plugins).each do |r|
   include_recipe "#{cookbook_name}::#{r}"
