1ae69faa6ae9fc570c95ff01773807b201f35922
Nathan Williams <nathan@teamtreehouse.com>
2015-08-12 20:54:04 -0700

    revert plugin, use upstream

--- test/fixtures/cookbooks/setup/attributes/plugin.rb
+++
@@ -1,9 +0,0 @@
-
-default['elasticsearch'].tap do |es|
-  es['skip_restart'] = false
-  es['config'].tap do |c|
-    c['cloud.aws.access_key'] = 'AKVAIQBF2RECL7FJWGJQ'
-    c['cloud.aws.secret_key'] = 'vExyMThREXeRMm/b/LRzEB8jWwvzQeXgjqMX+6br'
-    c['plugin.mandatory'] = 'elasticsearch-cloud-aws'
-  end
-end
--- test/fixtures/cookbooks/setup/recipes/plugin.rb
+++
@@ -1,7 +0,0 @@
-
-elasticsearch_plugin 'elasticsearch/elasticsearch-cloud-aws' do
-  version '2.7.0'
-  unless node['elasticsearch']['skip_restart']
-    notifies :restart, 'service[elasticsearch]', :delayed
-  end
-end
